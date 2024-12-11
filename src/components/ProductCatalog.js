d55dcfa34e9ac541e249287f9b9477c1ee58620d
Alex <megaoh478@gmail.com>
2024-12-11 17:12:37 +0200

    ex7

--- src/components/ProductCatalog.js
+++ src/components/ProductCatalog.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setProducts } from '../store/productsSlice';
 import { addToCart } from '../store/cartSlice';
-import './ProductCatalog.css';  
 
 const ProductCatalog = () => {
   const dispatch = useDispatch();
@@ -12,7 +11,7 @@ const ProductCatalog = () => {
     const fetchProducts = async () => {
       const response = await fetch('https://fakestoreapi.com/products');
       const data = await response.json();
-      dispatch(setProducts(data)); 
+      dispatch(setProducts(data)); // Оновлення списку товарів
     };
 
     fetchProducts();
@@ -39,7 +38,6 @@ const Product = ({ product }) => {
 
   return (
     <div className="product">
-      <img src={product.image} alt={product.title} className="product-image" />
       <h2>{product.title}</h2>
       <p>{product.description}</p>
       <p>${product.price}</p>
